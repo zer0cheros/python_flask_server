e9f0512127b3351ef11c9989006aa789e4c73a2e
Zer0cheros <christian.wiss@optimaedu.fi>
2022-01-18 15:10:51 +0200

    init

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 server = Flask(__name__)
 
 @server.route('/')
 def index():
-    return 'hello'
+    return render_template('index')
 
 if __name__ == '__main__':
     server.run(host='0.0.0.0', port=3000)
