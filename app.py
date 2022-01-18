0aca5a9c347aef7dcad9aa592ba448cd2ef84642
Zer0cheros <christian.wiss@optimaedu.fi>
2022-01-18 15:25:31 +0200

    added mongodb

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from distutils.log import debug
 from flask import Flask, render_template
+from flask_mongoengine import MongoEngine
 server = Flask(__name__)
+server.config['MONGODB_SETTINGS'] = {
+    'db': 'users',
+    'host': 'localhost',
+    'port': 27017
+}
+db = MongoEngine()
+db.init_app(server)
+
+class User(db.Document):
+    name = db.StringField()
+    email = db.StringField()
+
+User(name='Chris', email='chris@gmail.com').save()
 
 @server.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', user='Christian')
 
 if __name__ == '__main__':
     server.run(host='0.0.0.0', debug=True, port=3000)
